c19b946249c58dca22929059df5badb2e71dc059
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-05-08 11:00:46 +0200

    Added routes

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
 
+//Serve static images 
 app.use('/assets', express.static('hamsters'))
+
+//Routes
+const hamstersRoute = require('./routes/hamsters');
+const chartsRoute = require('./routes/charts');
+const gamesRoute = require('./routes/games');
+const statsRoute = require('./routes/stats');
+
+app.use('/hamsters', hamstersRoute);
+app.use('/charts', chartsRoute);
+app.use('/games', gamesRoute);
+app.use('/stats', statsRoute);
+
 
 app.listen(3000, () => {
     console.log('Server up and running!');
