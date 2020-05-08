78e28af9fd63f675eba1018d0456465f21a6190c
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-05-08 10:43:38 +0200

    Serve static image files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use('/assets', express.static('hamsters'))
+
 app.listen(3000, () => {
     console.log('Server up and running!');
 })
