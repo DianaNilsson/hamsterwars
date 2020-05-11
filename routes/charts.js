5198401100fc278411f2c45abc6de6065a4d6a5c
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-05-11 16:12:40 +0200

    Get top 5 winning and defeated hamsters

--- routes/charts.js
+++ routes/charts.js
@@ -1,8 +1,56 @@
 const {
+    db
+} = require('./../firebase');
+
+const {
     Router
 } = require('express');
 
 const router = new Router();
+
+//Get top 5 winning hamsters
+router.get('/top', async (req, res) => {
+
+    try {
+        let topHamsters = [];
+
+        //Find hamsters where id = req.params.id
+        let snapShot = await db.collection('hamsters').orderBy('wins', 'desc').limit(5).get()
+
+        //Loop through the snapShot array
+        snapShot.forEach(doc => {
+            topHamsters.push(doc.data());
+        })
+
+        res.status(200).send(topHamsters)
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+})
+
+//Get bottom 5 hamsters
+router.get('/bottom', async (req, res) => {
+
+    try {
+        let mostDefeatedHamsters = [];
+
+        //Find hamsters where id = req.params.id
+        let snapShot = await db.collection('hamsters').orderBy('defeats', 'desc').limit(5).get()
+
+        //Loop through the snapShot array
+        snapShot.forEach(doc => {
+            mostDefeatedHamsters.push(doc.data());
+        })
+
+        res.status(200).send(mostDefeatedHamsters)
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+})
 
 
 module.exports = router;
