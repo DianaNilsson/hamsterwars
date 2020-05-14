100a97c79a21c4ce1b10304977250b24aefc6f4b
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-05-14 14:10:19 +0200

    Get stats facts

--- routes/stats.js
+++ routes/stats.js
@@ -8,11 +8,10 @@ const {
 
 const router = new Router();
 
-//Get all total number of games
+//Get total number of games
 router.get('/total', async (req, res) => {
 
     try {
-
         let totalGames;
 
         //Get total number of games from fs (collection games, document counter)
@@ -25,7 +24,91 @@ router.get('/total', async (req, res) => {
             totalGames
         })
 
-    } catch {
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+})
+
+//Get funny hamster facts
+router.get('/facts', async (req, res) => {
+
+    try {
+
+        let age = []
+        let favFood = []
+        let favActivities = []
+
+        //Get all hamster docs from fs
+        let getHamsters = await db.collection('hamsters').get()
+
+        //Push hamster properties into different arrays
+        getHamsters.forEach(hamster => {
+            age.push(hamster.data().age)
+            favFood.push(hamster.data().favFood)
+            favActivities.push(hamster.data().loves)
+        })
+
+        //Get average age
+        function sumAge(total, num) {
+            return (total + num);
+        }
+        let averageAge = age.reduce(sumAge) / age.length
+
+        //Get most popular food
+        var distinctFood = {};
+        var mostPopularFood = {
+            count: 0,
+            food: null
+        };
+        for (let i = 0; i < favFood.length; i++) {
+            //If property does not exist in distinctFood create new property and asign 1
+            if (!distinctFood.hasOwnProperty(favFood[i])) {
+                distinctFood[favFood[i]] = 1;
+            }
+            // If property exist then increment its value
+            else {
+                distinctFood[favFood[i]]++;
+            }
+
+            // Set mostPopularFood to the object with highest count
+            if (distinctFood[favFood[i]] > mostPopularFood.count) {
+                mostPopularFood.count = distinctFood[favFood[i]];
+                mostPopularFood.food = favFood[i];
+            }
+        }
+
+        //Get most popular activity
+        var distinctActivity = {};
+        var mostPopularActivity = {
+            count: 0,
+            activity: null
+        };
+        for (let i = 0; i < favActivities.length; i++) {
+            //If property does not exist in distinctActivity create new property and asign 1
+            if (!distinctActivity.hasOwnProperty(favActivities[i])) {
+                distinctActivity[favActivities[i]] = 1;
+            }
+            // If property exist then increment its value
+            else {
+                distinctActivity[favActivities[i]]++;
+            }
+
+            // Set mostPopularActivity to the object with highest count
+            if (distinctActivity[favActivities[i]] > mostPopularActivity.count) {
+                mostPopularActivity.count = distinctActivity[favActivities[i]];
+                mostPopularActivity.activity = favActivities[i];
+            }
+        }
+
+        //Send response
+        res.status(200).send({
+            averageAge,
+            favouriteFood: mostPopularFood.food,
+            favouriteActivity: mostPopularActivity.activity
+        })
+
+    } catch (err) {
         console.log(err)
         res.status(500).send(err);
     }
