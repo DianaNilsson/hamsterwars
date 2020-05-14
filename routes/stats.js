3758c9672662ceccf1cb06f56e47c0280e5d2159
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-05-14 09:57:42 +0200

    Get stats total

--- routes/stats.js
+++ routes/stats.js
@@ -1,8 +1,36 @@
 const {
+    db
+} = require('./../firebase');
+
+const {
     Router
 } = require('express');
 
 const router = new Router();
+
+//Get all total number of games
+router.get('/total', async (req, res) => {
+
+    try {
+
+        let totalGames;
+
+        //Get total number of games from fs (collection games, document counter)
+        let getTotalGames = await db.collection('games').doc('counter').get()
+
+        //Set totalGames
+        totalGames = getTotalGames.data().gameCount;
+
+        res.status(200).send({
+            totalGames
+        })
+
+    } catch {
+        console.log(err)
+        res.status(500).send(err);
+    }
+
+})
 
 
 module.exports = router;
